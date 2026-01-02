f4cb758f5b8f4dabe29c22812b03806cea19b9cb
alitquan <alitmallick.gmail.com>
2026-01-02 16:58:42 -0500

    successfully retrieves the page number

--- extract.cpp
+++ extract.cpp
@@ -19,6 +19,8 @@ int readFile(std::string filename) {
     // 3 is missing since 3rd line after line break is typically empty space
 
     std::string linebreak = "==========";
+    std::string pageMarker= "page";
+    std::string dateMarker= "Added on";
     std::string line;
     std::string kTitle, kPage, kBody, kLoc; 
     // need kDate 
@@ -42,9 +44,29 @@ int readFile(std::string filename) {
         // acquire the title here 
         if (lineNumber == META) {
             std::cout << "META:" << line << std::endl;  
+            auto pgNumber = line.find("page"); 
+
+            // adding 5 because 'page' takes up 4 chars. There is also white space before page number
+            // "page 65" 
+            pgNumber += 5;
+
+            std::cout << "META -- DETECTED PAGE NUMBER         : " << pgNumber << std::endl;
+            std::cout << "META -- DETECTED PAGE NUMBER (charAt): " << line[pgNumber]<< std::endl;
+
+
+
+            auto pgDate   = line.find("Added on"); 
+            std::cout << "META -- DETECTED DATE        : " << pgDate << std::endl;
+
             // search through the line for page numbers and locations
             // loc might be available for non=pdfs
             // acquire the page number
+            std::string pgSubString = line.substr(pgNumber,pgDate); 
+            int pgNumberEnd = line.find(" ");
+            std::cout << "Page substring: " << pgSubString << std::endl;
+            int pgNum      = std::stoi(pgSubString.substr(0,pgNumberEnd+1));
+            std::cout << "Page number: " << pgNum << std::endl;
+            
             // acquire the date
         } 
         // acquire the body here 
@@ -58,6 +80,7 @@ int readFile(std::string filename) {
     } 
     return 0;
 } 
+
 
 int test1() {
     createFile("shawndaty.txt");
